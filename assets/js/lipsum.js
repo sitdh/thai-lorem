e7728b2dabbc366d005dfd8d014430caebb9b2bd
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-11-14 10:19:30 +0700

    Init class

--- assets/js/lipsum.js
+++ assets/js/lipsum.js
@@ -1,1 +1,20 @@
-console.log('hello');
+angular.module('tipsum', ['ui.bootstrap']).controller('GoldenMountain', ['$scope', '$log', '$http', function($scope, $log, $http) {
+
+    $scope.title = "ไทยลิปซั่ม";
+
+    var content = null;
+    $http.get('assets/data/data.json').success(function(response){
+        a = JSON.parse(response);
+        content = a;
+    });
+
+    $scope.generateType = 'plain';
+    $scope.repitation = 5;
+    $scope.dataGenerated = '';
+
+    $scope.generated = function() {
+        $log.log(content[0]);
+        $scope.dataGenerated = content[0];
+    }
+
+}]);
